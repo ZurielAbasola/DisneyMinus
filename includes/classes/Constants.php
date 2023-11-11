5c4ba9ae7d9fcb2d4ccbc3cd7460379e74951ce7
ZurielAbasola <zuriel123.abasola@gmail.com>
2023-11-11 13:20:56 -0700

    Form sanitizer

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -1,13 +1,15 @@
 <?php
 class Constants {
-    public static $firstNameCharacters = "Your first name must be between 2 and 25 characters.";
-    public static $lastNameCharacters = "Your last name must be between 2 and 25 characters.";
-    public static $usernameCharacters = "Your username must be between 5 and 25 characters.";
-    public static $usernameTaken = "This username is already taken.";
-    public static $emailsDoNotMatch = "Your emails don't match.";
-    public static $emailInvalid = "Your email is invalid.";
-    public static $emailTaken = "This email is already in use.";
-    public static $passwordsDoNotMatch = "Your passwords don't match.";
-    public static $passwordsLegnth = "Your password must be between 5 and 25 characters.";
-    public static $loginFailed = "Your username or password was incorrect.";
+    public static $firstNameCharacters = "Your first name must be between 2 and 25 characters";
+    public static $lastNameCharacters = "Your last name must be between 2 and 25 characters";
+    public static $usernameCharacters = "Your username must be between 2 and 25 characters";
+    public static $usernameTaken = "Username already in use";
+    public static $emailsDontMatch = "Your emails don't match";
+    public static $emailInvalid = "Invalid email";
+    public static $emailTaken = "Email already in use";
+    public static $passwordsDontMatch = "Passwords don't match";
+    public static $passwordsLength = "Your password must be between 2 and 25 characters";
+    public static $loginFailed = "Your username or password is incorrect";
+    public static $passwordIncorrect = "Your old password is incorrect";
 }
+?>
--- login.php
+++ login.php
@@ -1,2 +1,50 @@
 <?php
 require_once("includes/config.php");
+require_once("includes/classes/Account.php");
+require_once("includes/classes/Constants.php");
+require_once("includes/classes/FormSanitizer.php");
+// TODO: inlclude FormSanitizer class
+
+$account = new Account($con);
+
+if(isset($_POST["submitButton"])) {
+
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to DisneyMinus</title>
+    <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+</head>
+<body>
+
+<div class="signInContainer">
+
+    <div class="column">
+
+        <div class="header">
+            <img src="assets/images/logo.png" title="Logo" alt="Site logo" />
+            <h3>Sign In</h3>
+            <span>to continue to Netflux</span>
+        </div>
+
+        <form method="POST">
+            <?php echo $account->getError(Constants::$loginFailed); ?>
+            <input type="text" name="username" placeholder="Username" value="<?php getInputValue("username"); ?>" required>
+
+            <input type="password" name="password" placeholder="Password" required>
+
+            <input type="submit" name="submitButton" value="SUBMIT">
+
+        </form>
+
+        <a href="register.php" class="signInMessage">Need an account? Sign up here!</a>
+
+    </div>
+
+</div>
+
+</body>
+</html>
