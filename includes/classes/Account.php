12579edb52866d31bec977b63a790b40deaf05ad
ZurielAbasola <zuriel123.abasola@gmail.com>
2023-11-10 18:34:49 -0700

    Account stuff

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -7,15 +7,16 @@ class Account {
         $this->con = $con;
     }
     
-    public function register($fn, $ln, $em, $em2, $pw, $pw2) {
+    public function register($fn, $ln, $un, $em, $em2, $pw, $pw2) {
         $this->validateFirstName($fn);
         $this->validateLastName($ln);
+        $this->validateUsername($un);
         $this->validateEmails($em, $em2);
         $this->validatePasswords($pw, $pw2);
         
         if(empty($this->errorArray)) {
             // Insert into db
-            return $this->insertUserDetails($fn, $ln, $em, $pw);
+            return $this->insertUserDetails($fn, $ln, $un, $em, $pw);
         }
         else {
             return false;
@@ -39,6 +40,32 @@ class Account {
             return false;
         }
     }
+
+    public function insertUserDetails($fn, $ln, $un, $em, $pw) {
+        $pw = hash("sha512", $pw);
+
+        $query = $this->con->prepare("INSERT INTO users (firstName, lastName, username, email, password) 
+                                        VALUES (:fn, :ln, :un, :em, :pw)");
+        $query->bindValue(":fn", $fn);
+        $query->bindValue(":ln", $ln);
+        $query->bindValue(":un", $un);
+        $query->bindValue(":em", $em);
+        $query->bindValue(":pw", $pw);
+
+        return $query->execute();
+    }
+
+    private function validateFirstName($fn) {}
+
+    private function validateLastName($ln) {}
+
+    private function validateUsername($un) {}
+
+    private function validateEmails($em, $em2) {}
+
+    private function validatePasswords($pw, $pw2) {}
+
+    public function getError($error) {}
 }
 
 ?>
